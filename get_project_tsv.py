4c70de6184bb8e410b12f2f1b3e29e844bb6b228
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-04-04 15:15:48 -0700

    export project_id and page_urls; add code block for downloading files

--- get_project_tsv.py
+++ get_project_tsv.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from utils import get_project_links
 import time
 import os
+import requests
+import csv
 import glob
 
 t_start = time.time()
@@ -54,7 +56,6 @@ try:
 except NoSuchElementException:
     print("Searchbox is not found.")
 
-# TODO: need to add code block to click search button to exclude "Health" results
 try:
     search_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "span.input-group-btn button.btn.btn-default[type='submit']"))
@@ -100,6 +101,56 @@ while True:
         print(f"No next button found or an error occurred: {e}")
         break
 
-link_ct = sum([len(link) for link in all_project_links.values()]) # 04042025: 100 projects with 127 links on page 1 and 56 links on page 2
+link_ct = sum([len(link) for link in all_project_links.values()])
 print(f"Found {len(all_project_links)} projects with a total of {link_ct} links.")
-print(f"First project link: {all_project_links[0]}")
+# 04042025: Found 145 projects with a total of 183 links.
+
+# export all_project_links to csv
+proj_path = os.path.join(download_dir, "GMrepo_project_links.csv")
+with open(proj_path, "w", newline="", encoding="utf-8") as csv_f:
+    writer = csv.writer(csv_f)
+    writer.writerow(["Project_ID", "Page_Link"])
+    for project_id, links in all_project_links.items():
+        for link in links:
+            writer.writerow([project_id, link])
+print(f"Exported dictionary to {proj_path}")
+
+# download project phenotype files
+for _id, p_urls in all_project_links.items():
+    for p_url in p_urls:
+        print(f"Processing project {_id} from page: {p_url}")
+        driver.get(p_url)
+        time.sleep(2)
+
+        try:
+            dump_elem = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                    "//a[contains(text(), 'Download stats') and contains(@href, '.tsv.gz')]"))
+            )
+            dump_url = dump_elem.get_attribute("href")
+
+            resp = requests.get(dump_url, timeout=120)
+            resp.raise_for_status()
+
+            dump_fname = os.path.basename(dump_url)
+            new_fname = f"{_id}_{dump_fname}"
+            new_fpath = os.path.join(download_dir, new_fname)
+
+            with open(new_fpath, "wb") as f:
+                f.write(resp.content)
+            print(f"Downloaded {dump_fname} to {new_fpath}")
+        except Exception as e:
+            print(f"Failed to download {dump_fname} from page {p_url}: {e}")
+
+t_end = time.time()
+total_time = t_end - t_start
+print("Done.")
+
+tsv_file = glob.glob(f"{download_dir}/*.tsv.gz")
+if len(tsv_file) == link_ct:
+    print(f"All {len(tsv_file)} files are downloaded.")
+else:
+    print(f"{link_ct - len(tsv_file)} files are missing.")
+
+# Took about ~14 minutes to download all 183 files.
+print(f"Total time spent for download: {total_time/60} minutes.")
